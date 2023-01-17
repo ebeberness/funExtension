828c60f6ee8074b314498dc9f06983ff13c82d67
ebeberness <enb1@uw.edu>
2023-01-17 11:01:26 -0800

    created check boxes for glasses of water

---
+++ popup.js
@@ -0,0 +1,14 @@
+console.log("Drink Water!");
+
+
+function listen(eventType, selector, callback) {
+    document.querySelector(selector).addEventListener(eventType, callback);
+  }
+
+// function takes in an event type, i.e. click, moudsedown, etc.
+// takes in a selector, css selector, ID of different elements
+// Call back function
+  
+  
+  
+  
